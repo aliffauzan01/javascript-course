1398a047b5ba7d609b767e608f9a37548e15c6b4
Khoerul Rizal <rzl.trimelty@gmail.com>
2025-06-26 14:11:23 +0700

    feat: for in

--- javascript-dasar/break-continue.js
+++ javascript-dasar/break-continue.js
@@ -1,8 +1,8 @@
 let angka = 1;
 
 while (angka <= 10) {
-  if (angka === 5) {
-    console.log('Skip angka 5');
+  if (angka === 10) {
+    console.log('Skip angka ' + angka);
     angka++; // Jangan lupa untuk increment angka agar tidak masuk infinite loop
     continue; // Lanjutkan ke iterasi berikutnya
   }
---
+++ javascript-dasar/for-in.js
@@ -0,0 +1,11 @@
+const array = [1, 'dua', 3, 4, 5, 6, 7, 8, 9, 10];
+
+const object = {
+  nama: 'Budi',
+  umur: 30,
+};
+
+for (let index in object) {
+  console.log(index);
+  //   console.log(object[index]);
+}
