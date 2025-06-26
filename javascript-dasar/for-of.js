7b19f13f4b51b735f39771d722a8c70a42907138
Khoerul Rizal <rzl.trimelty@gmail.com>
2025-06-26 14:27:08 +0700

    feat: function

---
+++ javascript-dasar/for-of.js
@@ -0,0 +1,5 @@
+const array = ['a', 'b', 'c', 'd', 'e'];
+
+for (let value of array) {
+  console.log(value);
+}
---
+++ javascript-dasar/function.js
@@ -0,0 +1,9 @@
+function hitungAngka(a, b) {
+  console.log(a);
+  console.log(b);
+  console.log('Jumlah Angka: ' + (a + b));
+  return a + b;
+}
+
+let angka1 = hitungAngka(10, 20);
+console.log('Angka 1:', angka1);
