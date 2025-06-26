47192404644637c52a21bdb2401f6d73445faed9
Khoerul Rizal <rzl.trimelty@gmail.com>
2025-06-26 13:39:52 +0700

    feat: add for-loop

---
+++ javascript-dasar/truthy-falsy.js
@@ -0,0 +1,10 @@
+const falsy = null; // null is falsy
+const falsy1 = undefined; // undefined is falsy
+const falsy2 = 0; // 0 is falsy
+const falsy3 = ''; // empty string is falsy
+const falsy4 = false; // false is falsy
+const falsy5 = NaN; // NaN is falsy
+
+const truthy = ' '; // space string is truthy
+
+console.log(Boolean(boolean));
